4b19be9d2840b1cf6c62689b7a5f3359bb48bc01
mbergsto <mbergst@stud.ntnu.no>
2025-11-11 14:06:57 +0100

    minor api changes

--- literature-mining/scripts/test_config.py
+++ literature-mining/scripts/test_config.py
@@ -1,3 +1,5 @@
+from pybliometrics.scopus import AbstractRetrieval
 import pybliometrics
-
-print(pybliometrics.utils.constants.CONFIG_FILE)
+pybliometrics.scopus.init()
+d = AbstractRetrieval("2-s2.0-0034570550", id_type="eid", view="FULL", refresh=True)
+print("Retrieval quota left:", d.get_key_remaining_quota())
