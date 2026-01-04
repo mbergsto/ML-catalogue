bb8137915d7a106fe54846e6fb235649c7cb1def
mbergsto <mbergst@stud.ntnu.no>
2026-01-04 14:17:44 +0100

    readme

--- literature-mining/scripts/test_config.py
+++
@@ -1,5 +0,0 @@
-from pybliometrics.scopus import AbstractRetrieval
-import pybliometrics
-pybliometrics.scopus.init()
-d = AbstractRetrieval("2-s2.0-0034570550", id_type="eid", view="FULL", refresh=True)
-print("Retrieval quota left:", d.get_key_remaining_quota())
